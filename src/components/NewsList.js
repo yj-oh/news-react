e4bf1697bc9a6bed72b1b36b8bc460261e98d957
yj-oh <holidayoh99@gmail.com>
2020-08-24 01:00:21 +0900

    [기능] 뉴스 목록 구현

---
+++ src/components/NewsList.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+	padding: 1rem;
+	background-color: rgb(245, 245, 245);
+	color: #3c3c3c;
+	box-sizing: border-box;
+	
+	header > * {
+		margin: 0.5rem;
+	}
+`;
+
+const NewsList = () => {
+	const [datetime] = useState(new Date());
+	const [articles, setArticles] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+			try {
+				const response = await axios.get(
+					'https://newsapi.org/v2/top-headlines?country=kr&apiKey=28878c40f7874160af9a7113f65623aa',
+				);
+				setArticles(response.data.articles);
+			} catch (e) {
+				console.log(e);
+			}
+			setLoading(false);
+		};
+		fetchData();
+	}, []);
+
+	if(loading) {
+		return <NewsListBlock>Loading...</NewsListBlock>
+	}
+	if(!articles) {
+		return null;
+	}
+
+	return (
+		<NewsListBlock>
+			<header>
+				<h2>News</h2>
+				<p>{datetime.toLocaleDateString() + "  " + datetime.toLocaleTimeString()}</p>
+			</header>
+			{articles.map(article => (
+				<NewsItem key={article.url} article={article} />
+			))}
+		</NewsListBlock>
+	);
+};
+
+export default NewsList;
